40f3dd3860d81385d3cbf842777c45b136956782
Daniel Wuggenig <dwuggenig@anexia-it.com>
2020-07-14 15:30:53 +0200

    decorator fix

--- src/components/AnxHeader.vue
+++ src/components/AnxHeader.vue
@@ -5,7 +5,9 @@
 </template>
 
 <script lang="ts">
-import { Vue } from "vue-property-decorator";
+import { Vue, Component } from "vue-property-decorator";
 
+@Component( {
+})
 export default class AnxHeader extends Vue {}
 </script>
