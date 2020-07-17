d8ace1d1a5a038f375e82576dc11f6fd96a007f0
Melanie Klupper <mklupper@anexia-it.com>
2020-07-17 12:02:33 +0200

    add InputTextField (WIP)

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,16 @@ import VueI18n from 'vue-i18n'
 Vue.config.productionTip = false;
 Vue.use(VueI18n)
 
+import BootstrapVue from "bootstrap-vue";
+import App from "./App.vue";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import VeeValidate from "vee-validate";
+
+Vue.use(BootstrapVue);
+Vue.config.productionTip = false;
+Vue.use(VeeValidate);
+
 new Vue({
   render: h => h(App)
 }).$mount("#app");
