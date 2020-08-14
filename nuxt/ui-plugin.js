b8111b8a984ea4af92b8ffd2d4d7ca111c8a439c
pkrumpl <pkrumpl@anexia-it.com>
2020-08-14 08:12:18 +0200

    configure vue cli lib build

--- nuxt/ui-plugin.js
+++ nuxt/ui-plugin.js
@@ -2,5 +2,9 @@ import Vue from "vue";
 import UIPlugin from "ui";
 import { AnxIconsPlugin } from "ui";
 
+// TODO: remove console output
+console.log(UIPlugin);
+console.log(AnxIconsPlugin);
+
 Vue.use(UIPlugin);
-Vue.use(AnxIconsPlugin);
+//Vue.use(AnxIconsPlugin);
