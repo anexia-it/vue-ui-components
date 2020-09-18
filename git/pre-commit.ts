4d120868e1f390171c06251141c8f76c7e61dff3
pkrumpl <pkrumpl@anexia-it.com>
2020-09-18 11:35:59 +0200

    fix: pre-commit git script output

--- git/pre-commit.ts
+++ git/pre-commit.ts
@@ -1,12 +1,6 @@
 import simpleGit, {SimpleGit} from 'simple-git';
 const git: SimpleGit = simpleGit();
 
-console.log("Starting pre-commit git script\n============================\n")
-
 git.add("lib/", () => {
-  console.log("added lib folder to git");
   console.log(`\x1b[32mAdded files to the git index\x1b[0m`);
-
-  git.push();
-  console.log(`\x1b[32mSuccessfully pushed files\x1b[0m`);
 });
