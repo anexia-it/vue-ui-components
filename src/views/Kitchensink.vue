3a4db3ab2f17bb8346ac1af8e7ae27a43ffde75f
Patrick Krumpl <pkrumpl@anexia-it.com>
2020-11-20 15:06:51 +0100

    feat: description for anx-table passing data
    AMS-3183

--- src/views/Kitchensink.vue
+++ src/views/Kitchensink.vue
@@ -174,6 +174,10 @@
       </anx-content>
 
       <anx-content title="Tables" size="h2">
+        <anx-paragraph>
+          In this section the <i>&lt;anx-table&gt;</i> component is described.
+        </anx-paragraph>
+
         <anx-paragraph title="Simple table" size="h3">
           The table below is a very simple table to demonstrate, how less code
           you need to implement such a table.
@@ -202,6 +206,27 @@
           :hover="tableOptions.hover === true ? true : null"
           :noHeader="tableOptions.noHeader === true ? true : null"
         />
+
+        <anx-paragraph title="Passing data" size="h3">
+          See the code examples in the kitchensink for an example of how to pass
+          data to a <i>&lt;anx-table&gt;</i>.<br /><br />
+          In general, the <strong>items</strong> property is used to pass the
+          data of to the <i>&lt;anx-table&gt;</i>. The component automatically
+          creates columns with headers depending on the structure of the array
+          passed at <strong>items</strong>. So, passing an array of objects via
+          the <strong>item</strong> property to the table would be enough for
+          <i>&lt;anx-table&gt;</i> to work properly.<br /><br />
+          You also have the possibility to edit the names and the style of each
+          individual column. Therefore the <strong>columns</strong> property is
+          used. The columns property also is an array of objects. Each object
+          represents one column in the table (See table above).
+          <strong>width</strong>, <strong>align</strong> and the header for the
+          column <strong>name</strong> can be defined. Every column object must
+          have the <strong>index</strong> property. This property binds the data
+          of items to the respective column. (<strong>Note: </strong>
+          <i>&lt;anx-table&gt;</i> won't work properly if you only pass the
+          columns property and no items)
+        </anx-paragraph>
 
         <anx-paragraph title="Complex table" size="h3">
           The following table is a little bit more complex.<br /><br />
