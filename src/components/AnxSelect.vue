778b744d9313c57acb08fe9320a05567b38c5d18
Patrick Krumpl <pkrumpl@anexia-it.com>
2020-12-23 07:59:18 +0100

    fix: anx-select click box
    cursor pointer for whole anx-select

--- src/components/AnxSelect.vue
+++ src/components/AnxSelect.vue
@@ -6,11 +6,12 @@
     tag="div"
   >
     <div
+      @click="show = !show"
       class="anx-select"
       :class="{ is_invalid: error.length > 0 || errors.length > 0 }"
       :style="cssProps"
     >
-      <label :for="id + '1'"> {{ label }}</label>
+      <label :for="id + '1'" @click.prevent> {{ label }}</label>
       <select :id="id + '1'" :name="id" v-model="selected">
         <option
           v-for="option in options"
@@ -20,7 +21,7 @@
           {{ option.text }}</option
         >
       </select>
-      <div class="anx-select-div" @click="show = !show">
+      <div class="anx-select-div">
         {{ selectedText }}
       </div>
       <ul class="anx-select-options" :class="{ show: show }">
@@ -29,7 +30,7 @@
           :key="option.value"
           :rel="option.value"
           :class="{ active: selected === option.value }"
-          @click="select(option)"
+          @click.stop="select(option)"
         >
           {{ option.text }}
         </li>
@@ -206,6 +207,7 @@ export default class AnxSelect extends Vue {
   label {
     white-space: nowrap;
     width: auto;
+    cursor: pointer;
   }
 
   select {
