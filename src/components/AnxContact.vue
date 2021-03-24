7134ab1488a0344777f780694e0c76dc2a445c98
Patrick Krumpl <pkrumpl@anexia-it.com>
2021-03-24 18:11:09 +0100

    feat: anx-contact
    add component
    add route and view
    
    AMS-3382

---
+++ src/components/AnxContact.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="anx-contact">
+    <anx-content>
+      Has to be implemented!
+    </anx-content>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import AnxContent from "./AnxContent.vue";
+
+@Component({
+  components: {
+    AnxContent
+  }
+})
+export default class AnxContact extends Vue {}
+</script>
+
+<style lang="scss">
+@import "../assets/scss/_variables.scss";
+</style>
---
+++ src/views/Contact.vue
@@ -0,0 +1,66 @@
+<template>
+  <anx-global id="app">
+    <anx-header
+      :menus="items"
+      :i18n="$i18n"
+      :icon-url="null"
+      @localeChange="localeChange($event)"
+    >
+      <template v-slot:icon>
+        <img
+          alt="anx-header-logo"
+          v-bind:src="require('../assets/img/anexia-logo.svg')"
+        />
+      </template>
+    </anx-header>
+    <div class="space"></div>
+
+    <anx-container>
+      <anx-contact />
+    </anx-container>
+
+    <anx-footer
+      :links="[
+        { text: 'Impressum', link: '#' },
+        { text: 'AGB', link: '#' }
+      ]"
+    >
+      <template v-slot:icon>
+        <img
+          class="anx-logo-footer"
+          alt="anx-logo-footer"
+          v-bind:src="require('../assets/img/anexia-logo.svg')"
+          width="56px"
+          height="23px"
+        />
+      </template>
+    </anx-footer>
+  </anx-global>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import AnxContact from "@/components/AnxContact.vue";
+import AnxContainer from "@/components/AnxContainer.vue";
+import AnxFooter from "@/components/AnxFooter.vue";
+import AnxGlobal from "@/components/AnxGlobal.vue";
+import AnxHeader from "@/components/AnxHeader.vue";
+
+@Component({
+  components: {
+    AnxContact,
+    AnxContainer,
+    AnxFooter,
+    AnxGlobal,
+    AnxHeader
+  }
+})
+export default class Login extends Vue {
+  public items: Array<object> = [
+    { menu: "Kitchensink", link: "/kitchensink" },
+    { menu: "Login", link: "/login" },
+    { menu: "Crud", link: "/crud" },
+    { menu: "Contact", link: "/contact" }
+  ];
+}
+</script>
