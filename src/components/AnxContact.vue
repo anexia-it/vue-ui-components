fb22edb5ad6fdb48e72a40f8c663e2829f3e7be6
Patrick Krumpl <pkrumpl@anexia-it.com>
2021-03-25 11:50:14 +0100

    feat: anx-contact
    recaptcha
    inputs
    
    AMS-3382

--- src/components/AnxContact.vue
+++ src/components/AnxContact.vue
@@ -1,7 +1,32 @@
 <template>
   <div class="anx-contact">
-    <anx-content>
-      Has to be implemented!
+    <anx-content title="Contact">
+      <anx-paragraph size="h2">
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+      </anx-paragraph>
+      <anx-form>
+        <div class="input-inline">
+          <anx-input
+            name="first name"
+            rules="required"
+            label="First Name"
+            width="45%"
+          />
+          <anx-input
+            name="last name"
+            rules="required"
+            label="Last Name"
+            width="45%"
+            class="right"
+          />
+        </div>
+        <anx-input name="email" rules="required|email" label="Email" />
+        <anx-textarea />
+
+        <vue-recaptcha sitekey="djksdjk" />
+      </anx-form>
     </anx-content>
   </div>
 </template>
@@ -9,15 +34,37 @@
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
 import AnxContent from "./AnxContent.vue";
+import AnxForm from "./AnxForm.vue";
+import AnxInput from "./AnxInput.vue";
+import AnxParagraph from "./AnxParagraph.vue";
+import AnxTextarea from "./AnxTextarea.vue";
+import VueRecaptcha from "vue-recaptcha";
 
 @Component({
   components: {
-    AnxContent
+    AnxContent,
+    AnxForm,
+    AnxInput,
+    AnxParagraph,
+    AnxTextarea,
+    VueRecaptcha
   }
 })
 export default class AnxContact extends Vue {}
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import "../assets/scss/_variables.scss";
+
+.anx-contact {
+  .input-inline {
+    .anx-input {
+      display: inline-grid;
+
+      &.right {
+        float: right;
+      }
+    }
+  }
+}
 </style>
