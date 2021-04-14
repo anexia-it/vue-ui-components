9848d93c45348227b1921a42f47d6ae6621d5615
Patrick Krumpl <pkrumpl@anexia-it.com>
2021-04-14 13:16:47 +0200

    feat: implement google recaptcha
    grecaptcha in anx-contact
    add settings to contact page
    anx button implement disabled color
    
    AMS-3382

--- src/components/AnxContact.vue
+++ src/components/AnxContact.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="anx-contact">
     <anx-content title="Contact">
-      <anx-paragraph size="h2">
-        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
-        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-        voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+      <anx-paragraph v-if="this.$slots.default">
+        <slot />
       </anx-paragraph>
       <anx-form>
         <div class="input-inline">
@@ -25,14 +23,44 @@
         <anx-input name="email" rules="required|email" label="Email" />
         <anx-textarea />
 
-        <vue-recaptcha sitekey="djksdjk" />
+        <!-- invisible google recaptcha -->
+        <vue-recaptcha
+          v-if="useRecaptcha && invisibleCaptcha"
+          :sitekey="recaptchaSitekey"
+          :loadRecaptchaScript="true"
+          @verify="verifyCaptcha"
+          @error="errorCaptcha"
+        >
+          <anx-button width="100%">
+            Send
+          </anx-button>
+        </vue-recaptcha>
+
+        <!-- google recaptcha component -->
+        <vue-recaptcha
+          v-else-if="useRecaptcha"
+          :sitekey="recaptchaSitekey"
+          :loadRecaptchaScript="true"
+          @verify="verifyCaptcha"
+          @error="errorCaptcha"
+        /><!-- TODO: add margin bottom to button -->
+
+        <!-- submit button -->
+        <anx-button
+          v-if="!useRecaptcha || !invisibleCaptcha"
+          width="100%"
+          :disabled="!isButtonEnabled"
+        >
+          Send
+        </anx-button>
       </anx-form>
     </anx-content>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from "vue-property-decorator";
+import { Vue, Component, Prop } from "vue-property-decorator";
+import AnxButton from "./AnxButton.vue";
 import AnxContent from "./AnxContent.vue";
 import AnxForm from "./AnxForm.vue";
 import AnxInput from "./AnxInput.vue";
@@ -42,6 +70,7 @@ import VueRecaptcha from "vue-recaptcha";
 
 @Component({
   components: {
+    AnxButton,
     AnxContent,
     AnxForm,
     AnxInput,
@@ -50,7 +79,45 @@ import VueRecaptcha from "vue-recaptcha";
     VueRecaptcha
   }
 })
-export default class AnxContact extends Vue {}
+export default class AnxContact extends Vue {
+  /** The key for the google recaptcha V2. Will only be rendered, if porvided */
+  @Prop({ default: null }) recaptchaSitekey!: string | null;
+
+  /** implement an invisible captcha */
+  @Prop({ default: true }) invisibleCaptcha!: boolean;
+
+  /** Anx-Contact in demo-mode. Shows information on how to use and does not send an actual request */
+  @Prop({ default: false }) demoMode!: boolean; // TODO: implement demo mode: don't send api request when demo mode acitvated
+
+  private mounted() {
+    if (this.demoMode) {
+      console.error(
+        "You have activated the demo-mode for anx-contact. Disable it in production!"
+      );
+    }
+  }
+
+  /** Checks if the recaptcha should be used */
+  private useRecaptcha() {
+    return this.recaptchaSitekey === null ? false : true;
+  }
+
+  /** Returns if the button should be enabled or not */
+  private get isButtonEnabled() {
+    // TODO: check for errors in google recaptcha and check if the button should be enabled
+    return false;
+  }
+
+  private verifyCaptcha(res: any) {
+    // TODO: implement logic
+    console.log("verify captcha", res);
+  }
+
+  private errorCaptcha() {
+    // TODO: implement logic
+    console.error("captcha error");
+  }
+}
 </script>
 
 <style lang="scss" scoped>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -16,7 +16,28 @@
     <div class="space"></div>
 
     <anx-container>
-      <anx-contact />
+      <anx-content title="Settings">
+        <anx-paragraph>
+          <anx-checkbox
+            name="Invisible captcha"
+            v-model="invisibleCaptcha"
+            @input="setItem('invisibleCapctha', $event ? '1' : '0', true)"
+          />
+        </anx-paragraph>
+      </anx-content>
+
+      <!-- See here for example on how to use anx-contact. Don't set demo mode in production! -->
+      <anx-contact
+        :recaptcha-sitekey="recaptchaSitekey"
+        :demo-mode="true"
+        :invisible-captcha="invisibleCaptcha"
+      >
+        This is an example for the <strong>anx-contact</strong> component. You
+        can modify several properties of this component. See
+        <i>components/AnxContact.vue</i> for more information on the different
+        properties. VueRecaptcha is also supported and will only be initialised
+        if a site key is provided.
+      </anx-contact>
     </anx-container>
 
     <anx-footer
@@ -41,26 +62,56 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import AnxContact from "@/components/AnxContact.vue";
+import AnxContent from "@/components/AnxContent.vue";
+import AnxCheckbox from "@/components/AnxCheckbox.vue";
 import AnxContainer from "@/components/AnxContainer.vue";
 import AnxFooter from "@/components/AnxFooter.vue";
 import AnxGlobal from "@/components/AnxGlobal.vue";
+import AnxParagraph from "@/components/AnxParagraph.vue";
 import AnxHeader from "@/components/AnxHeader.vue";
 
 @Component({
   components: {
     AnxContact,
+    AnxContent,
+    AnxCheckbox,
     AnxContainer,
     AnxFooter,
     AnxGlobal,
+    AnxParagraph,
     AnxHeader
   }
 })
-export default class Login extends Vue {
+export default class Contact extends Vue {
+  recaptchaSitekey = "6LeOdakaAAAAAJfWBk1IqPphdzCJ8kqm_j97tD79"; // TODO: remove this private token and create one for anexia
+  invisibleCaptcha = true;
+
   public items: Array<object> = [
     { menu: "Kitchensink", link: "/kitchensink" },
     { menu: "Login", link: "/login" },
     { menu: "Crud", link: "/crud" },
     { menu: "Contact", link: "/contact" }
   ];
+
+  private beforeMount() {
+    this.initLocalStorage();
+  }
+
+  private reloadPage() {
+    location.reload();
+  }
+
+  private initLocalStorage() {
+    this.invisibleCaptcha =
+      window.localStorage.getItem("invisibleCapctha") === "1" ? true : false;
+  }
+
+  private setItem(name: string, value: string, reload = false) {
+    window.localStorage.setItem(name, value);
+
+    if (reload) {
+      this.reloadPage();
+    }
+  }
 }
 </script>
