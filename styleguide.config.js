730002cb2dd344b11caa7c8b5f69df13e82ee1bf
Patrick Krumpl <pkrumpl@anexia-it.com>
2021-06-04 16:22:17 +0200

    feat: build styleguide by default

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   components: "src/components/**/[A-Z]*.vue",
   usageMode: "expand",
+  styleguideDir: "dist",
   getExampleFilename(componentPath) {
     return componentPath.replace(/\.vue?$/, ".md");
   }
