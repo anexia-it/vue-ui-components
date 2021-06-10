55394f7945f41bcc35b24a34681314026b37ef61
Patrick Krumpl <pkrumpl@anexia-it.com>
2021-06-10 10:27:33 +0200

    fix: styleguidist docs path

--- styleguide.config.js
+++ styleguide.config.js
@@ -5,6 +5,7 @@ module.exports = {
   getExampleFilename(componentPath) {
     // The .md files are stored separately in the /docs folder
     componentPath = componentPath.replace(/src\\components/, "docs");
+    componentPath = componentPath.replace(/src\/components/, "docs");
     return componentPath.replace(/\.vue?$/, ".md");
   }
 };
