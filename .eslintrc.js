0060dc0636f295969f849d373eacc46411add874
Patrick Krumpl <pkrumpl@anexia-it.com>
2021-07-12 11:17:14 +0200

    fix: remove mocha config

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,15 +25,6 @@ module.exports = {
         "**/tests/unit/**/*.spec.{j,t}s?(x)"
       ],
       env: {
-        mocha: true
-      }
-    },
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
-      ],
-      env: {
         jest: true
       }
     }
