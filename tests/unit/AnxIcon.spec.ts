0b21f7665daacec1a349a46cf68dda85474bc88a
Patrick Krumpl <pkrumpl@anexia-it.com>
2021-07-12 17:22:49 +0200

    fix: anx-icon test

--- tests/unit/AnxIcon.spec.ts
+++ tests/unit/AnxIcon.spec.ts
@@ -1,22 +1,34 @@
-import { shallowMount } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
+import { AnxIconsPlugin } from "@/icons"
 import AnxIcon from "@/components/AnxIcon.vue";
+import Vue from 'vue';
+
+beforeEach(() => {
+  // For testing we have to install the AnxIconsPlugin manually
+  Vue.use(AnxIconsPlugin);
+});
 
 describe("AnxIcon.vue", () => {
-  it("renders component", () => {
-    const wrapper = shallowMount(AnxIcon);
+  it("renders component", async () => {
+    // We have to use mount here
+    const wrapper = await mount(AnxIcon);
 
     expect(wrapper.get("div.anx-icon").exists()).toBe(true);
   });
 
-  it("has correct properties", () => {
+  it("has correct properties and renders different icon", () => {
     const height = "10px", width = "20px";
     const icon = "information", title = "This is a test title";
     const margin = "25px";
-    const wrapper = shallowMount(AnxIcon, {
-      propsData: { height, width, icon, margin }
+    const wrapper = mount(AnxIcon, {
+      propsData: { height, width, icon, margin, title }
     });
 
-    expect(wrapper.get("div.anx-icon").attributes("aria-label")).toMatch(title);
-    expect(wrapper.get("anx-icon-" + icon).exists()).toBe(true);
-  })
+    const iconComponent = wrapper.get(".anx-icon-" + icon);
+    expect(iconComponent.exists()).toBe(true);
+    expect(iconComponent.attributes('style')).toContain("width: " + width);
+    expect(iconComponent.attributes('style')).toContain("height: " + height);
+    expect(iconComponent.attributes('style')).toContain("margin: " + margin);
+    expect(iconComponent.text()).toContain(title);
+  });
 });
