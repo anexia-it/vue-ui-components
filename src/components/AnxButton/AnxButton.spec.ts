b0b3ea43ca679fc6db575174f8577d43089c5783
Patrick Krumpl <pkrumpl@anexia-it.com>
2021-07-26 13:26:28 +0200

    test: anx-button
    AMS-3814

--- src/components/AnxButton/AnxButton.spec.ts
+++ src/components/AnxButton/AnxButton.spec.ts
@@ -21,4 +21,28 @@ describe("AnxButton.vue", () => {
 
     expect(wrapper.attributes("type")).toBe(btnType);
   });
+
+  it("emitts event on click", async () => {
+    const wrapper = shallowMount(AnxButton);
+    await wrapper.trigger("click");
+    await wrapper.vm.$nextTick();
+    expect(wrapper.emitted("click")).toBeTruthy();
+  });
+
+  it("generate css styles", () => {
+    const width = '100px', color = 'red', disabledColor = 'blue';
+    const wrapper = shallowMount(AnxButton, {
+      propsData: { 
+        width,
+        color,
+        disabledColor
+       }
+    });
+    
+    // @ts-ignore
+    const cssProps = wrapper.vm.cssProps;
+    expect(cssProps['--button-width']).toBe(width);
+    expect(cssProps['--button-color']).toBe(color);
+    expect(cssProps['--button-color-disabled']).toBe(disabledColor);
+  })
 });
