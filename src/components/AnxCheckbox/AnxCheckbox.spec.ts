13747d2549615ef307836647e180fc1ce49d5f11
Patrick Krumpl <pkrumpl@anexia-it.com>
2021-07-26 14:28:42 +0200

    test: anx-checkbox
    AMS-3815

--- src/components/AnxCheckbox/AnxCheckbox.spec.ts
+++ src/components/AnxCheckbox/AnxCheckbox.spec.ts
@@ -14,4 +14,36 @@ describe("AnxCheckbox.vue", () => {
     expect(wrapper.get("input").attributes("id")).toMatch(name);
     expect(wrapper.text()).toMatch(text);
   });
+
+  it("emitts event on click", async () => {
+    const wrapper = shallowMount(AnxCheckbox);
+    const input = wrapper.get("input");
+    expect(input.exists()).toBeTruthy();
+    await input.trigger("click");
+    await wrapper.vm.$nextTick();
+    expect(wrapper.emitted("input")).toBeTruthy();
+  });
+
+  it("changes state on property change", async () => {
+    const wrapper = shallowMount(AnxCheckbox, {
+      propsData: {
+        value: false
+      }
+    });
+    
+    // @ts-ignore
+    expect(wrapper.get("input").element.checked).toBeFalsy();
+
+    // Check checkbox
+    wrapper.setProps({ value: true });
+    await wrapper.vm.$nextTick();
+    // @ts-ignore
+    expect(wrapper.get("input").element.checked).toBeTruthy();
+    
+    // Uncheck checkbox
+    wrapper.setProps({ value: false });
+    await wrapper.vm.$nextTick();
+    // @ts-ignore
+    expect(wrapper.get("input").element.checked).toBeFalsy();
+  })
 });
