f9bf0d52b6a09e4b296c4c0aefff87fb3793a192
Patrick Krumpl <pkrumpl@anexia-it.com>
2021-07-27 13:28:26 +0200

    test: anx-link
    AMS-3820

--- src/components/AnxLink/AnxLink.spec.ts
+++ src/components/AnxLink/AnxLink.spec.ts
@@ -1,5 +1,8 @@
-import { shallowMount } from "@vue/test-utils";
+import { createLocalVue, shallowMount } from "@vue/test-utils";
 import { AnxLink } from "@/components";
+import VueRouter from "vue-router";
+
+const Url = require("url-parse");
 
 describe("AnxLink.vue", () => {
   it("renders component", () => {
@@ -27,7 +30,7 @@ describe("AnxLink.vue", () => {
     expect(linkComponent.classes("no-style")).toBe(true);
   });
 
-  it("check for click event", async () => {
+  it("emitts click event", async () => {
     const wrapper = shallowMount(AnxLink);
     const linkComponent = wrapper.get("a.anx-link");
 
@@ -35,7 +38,7 @@ describe("AnxLink.vue", () => {
     expect(wrapper.emitted("click")).toBeTruthy();
   });
 
-  it("checks disabled functionality", async () => {
+  it("has disabled functionality", async () => {
     const wrapper = shallowMount(AnxLink, {
       propsData: { disabled: true }
     });
@@ -52,4 +55,260 @@ describe("AnxLink.vue", () => {
     await disabledLink.trigger("click");
     expect(wrapper.emitted("click")).toBeFalsy();
   });
+
+  it("opens internal url in same tab with vue router defined", async () => {
+    // Setting up VueRouter
+    const router = new VueRouter();
+
+    // Mocking the VueRouter push function that should be used
+    router.push = jest.fn();
+
+    // Properties for AnxLink
+    const href = "/test";
+
+    // Create the wrapper
+    const wrapper = createWrapperWithVueRouter({ href }, router);
+
+    // Check if the link has been rendered
+    const link = wrapper.get("a.anx-link");
+    expect(link.exists()).toBeTruthy();
+
+    // Click the link
+    await link.trigger("click");
+    await wrapper.vm.$nextTick();
+
+    // Check if the url is correct and the router has pushed the user to the location
+    expect(router.push).toHaveBeenCalledWith(href);
+  });
+
+  it("opens internal url in new tab with vue router defined", async () => {
+    // Mocking the window.open function that will be used
+    window.open = jest.fn();
+
+    // Setting up VueRouter
+    const router = new VueRouter();
+
+    // Properties for AnxLink
+    const href = "/test", newTab = true;
+
+    // Create the wrapper
+    const wrapper = createWrapperWithVueRouter({ href, newTab }, router);
+
+    // Check if the link has been rendered
+    const link = wrapper.get("a.anx-link");
+    expect(link.exists()).toBeTruthy();
+
+    // Click the link
+    await link.trigger("click");
+    await wrapper.vm.$nextTick();
+
+    // Check if the url is correct and a new tab has been opened
+    const expectedUrl = new Url(router.resolve(href).href).href;
+    expect(window.open).toHaveBeenCalledWith(expectedUrl);
+  });
+
+  it("opens route with name in same tab when vue router is defined", async () => {
+    // Setting up VueRouter
+    const router = new VueRouter();
+
+    // Mocking the VueRouter push function that should be used
+    router.push = jest.fn();
+
+    // Properties for AnxLink
+    const routeName = "Test";
+
+    // Create the wrapper
+    const wrapper = createWrapperWithVueRouter({ routeName }, router);
+
+    // Check if the link has been rendered
+    const link = wrapper.get("a.anx-link");
+    expect(link.exists()).toBeTruthy();
+
+    // Click the link
+    await link.trigger("click");
+    await wrapper.vm.$nextTick();
+
+    // Check if the url is correct and the router has pushed the user to the location
+    expect(router.push).toHaveBeenCalledWith({ name: routeName });
+  });
+
+  it("opens route with name in new tab when vue router is defined", async () => {
+    // Mocking the window.open function that will be used
+    window.open = jest.fn();
+
+    // Setting up routes
+    const routes = [
+      {
+        name: 'Test',
+        path: '/test'
+      },
+      {
+        name: 'Home',
+        path: '/'
+      }
+    ]
+
+    // Setting up VueRouter
+    const router = new VueRouter({ routes });
+
+    // Properties for AnxLink
+    const routeName = routes[0].name, newTab = true;
+
+    // Create the wrapper
+    const wrapper = createWrapperWithVueRouter({ routeName, newTab }, router)
+
+    // Check if the link has been rendered
+    const link = wrapper.get("a.anx-link");
+    expect(link.exists()).toBeTruthy();
+
+    // Click the link
+    await link.trigger("click");
+    await wrapper.vm.$nextTick();
+
+    // Check if the url is correct and the router has pushed the user to the location
+    const expectedUrl = new Url(router.resolve({ name: routes[0].name }).href).href;
+    expect(window.open).toHaveBeenCalledWith(expectedUrl);
+  });
+
+  it("doesn't push when route name is current url", async () => {
+    // Mocking the window.open function
+    window.open = jest.fn();
+
+    // Setting up routes
+    const routes = [
+      {
+        name: 'Test',
+        path: '/test'
+      },
+      {
+        name: 'Home', // This is the default route
+        path: '/'
+      }
+    ]
+
+    // Setting up VueRouter
+    const router = new VueRouter({ routes });
+
+    // Mocking the VueRouter push function that should be used
+    router.push = jest.fn();
+
+    // Properties for AnxLink
+    const routeName = routes[1].name;
+
+    // Create the wrapper
+    const wrapper = createWrapperWithVueRouter({ routeName }, router)
+
+    // Check if the link has been rendered
+    const link = wrapper.get("a.anx-link");
+    expect(link.exists()).toBeTruthy();
+
+    // Click the link
+    await link.trigger("click");
+    await wrapper.vm.$nextTick();
+
+    // Neither push nor window.open should have been called
+    expect(router.push).toHaveBeenCalledTimes(0);
+    expect(window.open).toHaveBeenCalledTimes(0);
+  });
+
+  it("doesn't open url with same route as current window in current window when router is defined", async () => {
+    // Mocking the window.open function
+    window.open = jest.fn();
+
+    // Setting up VueRouter
+    const router = new VueRouter();
+
+    // Mocking the VueRouter push function that should be used
+    router.push = jest.fn();
+
+    // Properties for AnxLink
+    const href = window.location.href;
+
+    // Create the wrapper
+    const wrapper = createWrapperWithVueRouter({ href }, router)
+
+    // Check if the link has been rendered
+    const link = wrapper.get("a.anx-link");
+    expect(link.exists()).toBeTruthy();
+
+    // Click the link
+    await link.trigger("click");
+    await wrapper.vm.$nextTick();
+
+    // Neither push nor window.open should have been called
+    expect(router.push).toHaveBeenCalledTimes(0);
+    expect(window.open).toHaveBeenCalledTimes(0);
+  });
+
+  it("opens url in new tab if href matches the current href when vue router is defined", async () => {
+    // Mocking the window.open function
+    window.open = jest.fn();
+
+    // Setting up VueRouter
+    const router = new VueRouter();
+
+    // Mocking the VueRouter push function that should be used
+    router.push = jest.fn();
+
+    // Properties for AnxLink
+    const href = window.location.href, newTab = true;
+
+    // Create the wrapper
+    const wrapper = createWrapperWithVueRouter({ href, newTab }, router)
+
+    // Check if the link has been rendered
+    const link = wrapper.get("a.anx-link");
+    expect(link.exists()).toBeTruthy();
+
+    // Click the link
+    await link.trigger("click");
+    await wrapper.vm.$nextTick();
+
+    // Neither push nor window.open should have been called
+    const expectedUrl = new Url(window.location.href).href;
+    expect(window.open).toHaveBeenCalledWith(expectedUrl)
+  });
+
+  it("opens url in same tab if href includes hostname and is internal url when vue router is defined", async () => {
+    // Setting up VueRouter
+    const router = new VueRouter();
+
+    // Mocking the VueRouter push function that should be used
+    router.push = jest.fn();
+
+    // Properties for AnxLink
+    const href = new Url('/test').href
+
+    // Create the wrapper
+    const wrapper = createWrapperWithVueRouter({ href }, router)
+
+    // Check if the link has been rendered
+    const link = wrapper.get("a.anx-link");
+    expect(link.exists()).toBeTruthy();
+
+    // Click the link
+    await link.trigger("click");
+    await wrapper.vm.$nextTick();
+
+    // Neither push nor window.open should have been called
+    expect(router.push).toHaveBeenCalledWith('/test')
+  });
 });
+
+function createWrapperWithVueRouter(propsData: {}, router: VueRouter | null = null) {
+  // Setting up VueRouter in local vue
+  const localVue = createLocalVue();
+  localVue.use(VueRouter);
+  if (router === null) {
+    router = new VueRouter();
+  }
+
+  // Create the AnxLink
+  const wrapper = shallowMount(AnxLink, {
+    propsData,
+    localVue,
+    router
+  });
+
+  return wrapper;
+}
