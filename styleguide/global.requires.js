4a2a22609d90a6f7d0f405dafc1c054e8b79bc31
Patrick Krumpl <pkrumpl@anexia-it.com>
2021-07-27 10:25:56 +0200

    test: anx-select
    AMS-3822

--- styleguide/global.requires.js
+++ styleguide/global.requires.js
@@ -2,11 +2,13 @@ import Vue from "vue";
 import { AnxIconsPlugin, AnxIconsNames, AnxToastPlugin } from "../src/plugins";
 import VueI18n from "vue-i18n";
 import Router from "./router.js";
+import VeeValidate from "vee-validate";
 
 /** Register the AnxIconsPlugin (this registers all icons as components) */
 Vue.use(AnxIconsPlugin);
 Vue.use(AnxToastPlugin);
 Vue.use(Router);
 Vue.use(VueI18n);
+Vue.use(VeeValidate);
 
 export { AnxIconsNames };
