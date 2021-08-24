966205c70eed4357167056e3adf1564bf1fef80d
Patrick Krumpl <pkrumpl@anexia-it.com>
2021-08-24 12:49:45 +0200

    fix: lint

--- src/lib/models/AbstractModel.d.ts
+++ src/lib/models/AbstractModel.d.ts
@@ -1,10 +1,10 @@
 export declare class AbstractModel {
-    public id: number | null;
-    protected static API_BASE: string;
-    protected static HOST: string;
-    static getAll(): Array<AbstractModel>;
-    static get(): AbstractModel;
-    public save(): void;
-    public delete(): void;
-    public update(): void;
+  public id: number | null;
+  protected static API_BASE: string;
+  protected static HOST: string;
+  static getAll(): Array<AbstractModel>;
+  static get(): AbstractModel;
+  public save(): void;
+  public delete(): void;
+  public update(): void;
 }
